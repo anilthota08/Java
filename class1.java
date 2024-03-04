92bbb937aba4ce9e14f92bf38326e9e0f978fd0f
anilthota08 <161741921+anilthota08@users.noreply.github.com>
2024-03-04 09:32:02 -0600

    Add files via upload

---
+++ class1.java
@@ -0,0 +1,36 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class class1 {
+    public static void main(String[] args) {
+        byte a=65;
+        short b=a;
+        System.out.println(b);
+        char c= (char)b;
+        System.out.println(c);
+        int d=c;
+        System.out.println(d);
+        long e=d;
+        System.out.println(e);
+        float f= e;
+        System.out.println(f);
+        double g=f;
+        System.out.println(g);
+        double h=65;
+        float i= (float) h;
+        System.out.println(i);
+        long j= (long)i;
+        System.out.println(j);
+        int k= (int)j;
+        System.out.println(k);
+        char l=(char)k;
+        System.out.println(l);
+        short m= (short)l;
+        System.out.println(m);
+        byte n= (byte)m;
+        System.out.println(n);
+
+
+
+    }
+}
+
